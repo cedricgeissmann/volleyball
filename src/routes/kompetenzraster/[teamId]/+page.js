446d27a00b85c9ce2cff6233ba76287955e2e4ce
Cedric Geissmann <cedric.geissmann@gmail.com>
2026-04-09 15:03:39 +0200

    Fix teams.

--- src/routes/kompetenzraster/[teamId]/+page.js
+++ src/routes/kompetenzraster/[teamId]/+page.js
@@ -1,4 +1,4 @@
-import { loadTeamById } from '$lib/utils/contentLoader.js';
+import { loadTeams } from '$lib/utils/contentLoader.js';
 import { getTeamRoutes } from '$lib/utils/routes.js';
 import { error } from '@sveltejs/kit';
 
@@ -8,7 +8,8 @@ export const entries = getTeamRoutes;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	const team = await loadTeamById(params.teamId);
+	const teams = await loadTeams();
+	const team = teams.find((t) => t.id === params.teamId);
 
 	if (!team) {
 		throw error(404, 'Team nicht gefunden');
